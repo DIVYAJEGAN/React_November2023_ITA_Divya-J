01f2420fad98c4af88b07b7790aacdc1559d6861
DIVYA J <124855134+DIVYAJEGAN@users.noreply.github.com>
2023-11-17 11:49:09 +0530

    DAY4

---
+++ day4/Reactapp2src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ButtonAppBar from './component/ButtonAppBar';
+function App() {
+  return (
+    <div className="App">
+     <ButtonAppBar/>
+    </div>
+  );
+}
+
+export default App;
